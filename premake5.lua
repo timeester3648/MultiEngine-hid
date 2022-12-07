8bb0bce0c3c3ccca63562b528b03e21589444a76
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:21 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -22,22 +22,3 @@ project "hid"
 
  	filter "system:windows"
 		files { "./windows/**.c" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
